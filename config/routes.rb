cd5111f19e89ccd686a3f2e8182f3ba2bb09b86a
Bohemian Codes <chavelitaloca@gmail.com>
2017-10-01 13:55:22 +0200

    created new controller -> demo

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+
+  root 'static_pages#index'
+
+  get 'demo/index'
+
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
   get 'static_pages/index'
-  root 'static_pages#index'
+
+  # default route
+  # may go away in future versions of Rails
+  # get ':controller (/:action (/:id))'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
