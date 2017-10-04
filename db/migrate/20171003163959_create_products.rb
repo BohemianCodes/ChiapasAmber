35191d260706fed30370a18a3e973b79760ff2bf
Bohemian Codes <chavelitaloca@gmail.com>
2017-10-04 10:05:08 +0200

    styled layout for products pages

--- db/migrate/20171003163959_create_products.rb
+++ db/migrate/20171003163959_create_products.rb
@@ -1,11 +1,18 @@
 class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
+
+      #shorthand for t.column "name" :string
       t.string :name
       t.text :description
       t.string :image_url
 
+      #we do not need to add a column for "id", this is done automatically
+
       t.timestamps
+      #shorthand for
+      #t.datetime "created_at"
+      #t.datetime "updated_at"
     end
   end
 end
