cffd177a233a519c35547d1e735283d85f759d6f
Bohemian Codes <chavelitaloca@gmail.com>
2017-10-05 18:04:05 +0200

    uploading for migration error review

---
+++ db/migrate/20171005151056_add_price_field.rb
@@ -0,0 +1,5 @@
+class AddPriceField < ActiveRecord::Migration[5.1]
+  def change
+    add_column :products, :price, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005084237) do
+ActiveRecord::Schema.define(version: 20171005153735) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20171005084237) do
     t.datetime "updated_at", null: false
     t.string "colour"
     t.string "string"
+    t.string "price"
   end
 
 end
