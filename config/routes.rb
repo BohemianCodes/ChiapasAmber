05176d228175504386f6dd2dd832b26ce0bccc29
Bohemian Codes <chavelitaloca@gmail.com>
2017-10-07 08:58:30 +0200

    created new controller "orders"

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :products
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
 
   get 'demo/index'
 
@@ -11,9 +11,11 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+  resources :orders, only: [:index, :show, :create, :destroy]
+
   # default route
   # may go away in future versions of Rails
   # get ':controller (/:action (/:id))'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
