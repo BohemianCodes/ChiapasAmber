e5657909aa3b2cb985358beaa0f55dabf991d851
Bohemian Codes <chavelitaloca@gmail.com>
2017-10-18 15:51:43 +0200

    adding search form to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
-  root 'products#index'
+  root 'static_pages#landing_page'
 
   resources :products
 
   get 'static_pages/about'
   get 'static_pages/contact'
   get 'static_pages/index'
-  get 'static_pages/landing_page'
+
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
