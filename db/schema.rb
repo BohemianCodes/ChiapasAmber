ad3d5e0a7d4f95545dce240f4265143a344177c9
Bohemian Codes <chavelitaloca@gmail.com>
2017-10-29 13:00:31 +0100

    add timestamps to orders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024194638) do
+ActiveRecord::Schema.define(version: 20171029115841) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
     t.integer "product_id"
     t.float "total"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["product_id"], name: "index_orders_on_product_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
