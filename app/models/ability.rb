5bc421b064611eba64cb71d41f18c385074a5ac7
Bohemian Codes <chavelitaloca@gmail.com>
2017-10-31 17:49:37 +0100

    adding admin flag for users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,6 +6,8 @@ class Ability
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
 
+    can :delete, Comment
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
