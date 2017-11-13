5d6d60d91cbac9c7f0fd44b34a7cd4d078f848d6
Bohemian Codes <chavelitaloca@gmail.com>
2017-11-13 17:12:38 +0100

    added stripe test payments

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,6 @@
-  Rails.configuration.stripe = {
+Rails.configuration.stripe = {
     publishable_key: 'pk_test_IHSstH3che6IAbFoWNf5t8GZ',
     secret_key: 'sk_test_gCWa3btEiHMbIWZWkbexnEm5'
   }
-end
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
