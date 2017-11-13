65af1f1c3cc52c5e18e4eeeb263484853e6e3b8d
Bohemian Codes <chavelitaloca@gmail.com>
2017-11-13 15:44:18 +0100

    using Stripe API test keys also for production env

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,9 +1,3 @@
-if Rails.env.production?
-  Rails.configuration.stripe = {
-    publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
-    secret_key: ENV['STRIPE_SECRET_KEY']
-  }
-else
   Rails.configuration.stripe = {
     publishable_key: 'pk_test_IHSstH3che6IAbFoWNf5t8GZ',
     secret_key: 'sk_test_gCWa3btEiHMbIWZWkbexnEm5'
