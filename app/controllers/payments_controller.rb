177228551fa5428fec4ec0011afb0bef3c729f4b
Bohemian Codes <chavelitaloca@gmail.com>
2017-11-16 17:02:49 +0100

    redirecting user to login page - if he is not logged in - before creating a charge

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,7 +2,11 @@ class PaymentsController < ApplicationController
 
   def create
     @product = Product.find(params[:product_id])
-    @user = current_user
+    if user_signed_in?
+      @user = current_user
+    else
+        redirect_to  new_user_registration_path
+    end
     token = params[:stripeToken]
   # Create the charge on Stripe's servers - this will charge the user's card
     begin
