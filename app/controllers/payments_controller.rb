6d6745f4485d7fe0217c827ad64494d61257bff1
Bohemian Codes <chavelitaloca@gmail.com>
2017-11-16 12:48:06 +0100

    setup Stripe changes, creating an Order and send receipt emails

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,34 @@
+class PaymentsController < ApplicationController
+
+  def create
+    @product = Product.find(params[:product_id])
+    @user = current_user
+    token = params[:stripeToken]
+  # Create the charge on Stripe's servers - this will charge the user's card
+    begin
+      charge = Stripe::Charge.create(
+        amount: (@product.price * 100).to_i, # amount in cents
+        currency: "eur",
+        source: token,
+        description: "Purchase",
+        receipt_email: params[:stripeEmail]
+      )
+
+    if charge.paid
+      Order.create(
+        user_id: @user,
+        product_id: @product,
+        total: @product.price
+      )
+    end
+
+    rescue Stripe::CardError => e
+    # The card has been declined
+    body = e.json_body
+    err = body[:error]
+    flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+  end
+
+    redirect_to product_path(@product)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   get 'static_pages/index'
   post 'static_pages/thank_you'
 
+  post 'payments/create'
+
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
