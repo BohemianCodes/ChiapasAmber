510fad00c10fd72f383436938c8254d108928d1d
Bohemian Codes <chavelitaloca@gmail.com>
2017-11-24 13:15:20 +0100

    Redis

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,8 @@ module ChiapasAmber
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    #Redis config
+    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
   end
 end
